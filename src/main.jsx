d1b47af1fa90e2e50754c1bb36596ac6fa96a960
ColeCarmody <colecarmody320@gmail.com>
2026-08-24 20:26:23 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CollegeHub from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CollegeHub />
+  </React.StrictMode>
+);
